ae96364e70efadfa65f2f7e1986927f3aad8878e
thinhphungdev <phungthinh0946519356@gmail.com>
2024-01-02 14:27:53 +0700

    setup webpack module federation plugin

--- products/src/index.js
+++ products/src/index.js
@@ -4,8 +4,7 @@ let products = '';
 
 for (let i = 0; i < 5; i++) {
   const name = faker.commerce.productName();
-
   products += `<div>${name}</div>`;
 }
 
-document.getElementById('dev-products').innerHTML = products;
+document.querySelector('#dev-products').innerHTML = products;
