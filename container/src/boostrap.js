2c65a5edd0aa1a4feac402f24017ee5a04fbe541
thinhphungdev <phungthinh0946519356@gmail.com>
2024-01-03 13:50:12 +0700

    refactor the cart

--- container/src/boostrap.js
+++ container/src/boostrap.js
@@ -1,4 +1,7 @@
-import 'products/ProductsIndex';
-import 'cart/CartShow';
+import { mount as productMount } from 'products/ProductsIndex';
+import { mount as cartMount } from 'cart/CartShow';
 
 console.log('Container');
+
+productMount(document.getElementById('my-product'));
+cartMount(document.getElementById('my-cart'));
